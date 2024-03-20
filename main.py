191ae94cc43457ed85965d63de3010a707cf2ec0
탱탱 <131268845+wvtayonkim@users.noreply.github.com>
2024-03-20 15:34:51 +0900

    main.py 업로드

---
+++ main.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+b = np.array([0,4,1]) # r vector (-1.5 feet)
+c = np.array([5,6,0]) # p cos(gama=30), p sin(gama=30), 0
+
+cross_result = np.cross(b,c)
+
+#print the cross-product result
+print("cross_result =", cross_result)
+
+a = np.array([2,0,3])
+
+scaler_triple = np.dot(a,cross_result)
+
+#print the scaler_triple result
+print("scaler_triple =", scaler_triple)
